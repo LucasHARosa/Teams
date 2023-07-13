0bd391371298446d2087f1022743ba2e553aab72
lucas rosa <l.ucas.henrique.a.r@gmail.com>
2023-07-13 15:27:08 -0300

    local storage groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,20 +2,34 @@ import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Turma do Fundão', 'Turma do Barulho']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation()
 
   function handleNewGroup() {
-    navigation.navigate('new')
+    navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]))
   return (
     <Container>
       <Header />
